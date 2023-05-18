692fc344e2a88e8e7328f18ebeb2de0e6f57901b
Razak Ayariga <razakayariga@yahoo.com>
2023-05-19 04:11:42 +1300

    Admin controller  and routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,2 +1,14 @@
-const express = ("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+
+const { 
+    registerAdminController,
+    adminLoginController
+    } = require("../controllers/adminController");
+
+
+    router.post("/registerAdmin", registerAdminController);
+    router.post("/adminLogin", adminLoginController);
+
+
+    module.exports = router
