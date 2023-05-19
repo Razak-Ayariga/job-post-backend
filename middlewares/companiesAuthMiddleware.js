23a9bf9a8b6820fdf50006752d2658ac835a165a
Swanzy Benjamin <benjaminswanzy@outlook.com>
2023-05-19 11:47:28 +0000

    secret

--- middlewares/companiesAuthMiddleware.js
+++ middlewares/companiesAuthMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const jwtSign = process.env.JWT_SECRET;
+const jwtSecret = process.env.JWT_SECRET;
 
 
 //middleware to generate company jwt
