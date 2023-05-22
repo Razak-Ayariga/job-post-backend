6fc19ba32c3bc82f42e8c11d48817ad715050e34
princetindan <wolfsontindan@gmail.com>
2023-05-22 11:19:27 +0000

    Changes merged

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const {sequelize} = require("./dataBase/dbConfig");
+const port = process.env.PORT || 4000;
+
+//body parser
+app.use(express.json());
+
+//import routes
+const jobSeekerRoutes = require("./routes/jobSeekerRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const CompanyRoutes = require("./routes/companyRoutes");
+
+//use routes
+app.use("/jobSeeker", jobSeekerRoutes);
+app.use("/admin", adminRoutes);
+app.use("/company", CompanyRoutes);
+
+
+
+(async () => {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection established successfully");
+  
+      app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+      });
+    } catch (error) {
+      console.log(error);
+      console.log("Unable to connect to the database");
+    }
+  })();
+
+
