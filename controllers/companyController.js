d635c477a39ec7ed422bc4e9af7519d5c17a3928
Razak Ayariga <razakayariga@yahoo.com>
2023-06-07 20:49:44 +1300

    company controller

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -20,21 +20,10 @@ const registerCompany = async (req, res) => {
     newCompany["password"] = hashPassword;
     newCompany["uuid"] = uuid;
 
-    const addCompany = await companyModel.create(newCompany, {
-      fields: [
-        "id",
-        "company_name",
-        "password",
-        "email",
-        "mobile_number",
-        "verification_method",
-      ],
-    });
+    const addCompany = await companyModel.create(newCompany, { fields: ["id", "company_name", "password", "email", "mobile_number", "verification_method"] });
     const company = addCompany.dataValues;
     console.log(company);
-    res
-      .status(201)
-      .json({ message: "Company registered successfully!", token, company });
+    res.status(201).json({ message: "Company registered successfully!", token, company });
     return;
   } catch (error) {
     res.status(500).json({ message: "Failed to register company!" });
@@ -44,20 +33,14 @@ const registerCompany = async (req, res) => {
 // Company login
 const companyLoginController = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email } = req.body;
     const token = req.token;
     const company = req.body;
 
     // Check if company exists
-    const findUser = await companyModel.findOne({ where: { email: email } });
+    const findUser = await companyModel.findOne({where: { email:email }});
     if (!findUser) {
-      return res
-        .status(403)
-        .json({ message: "Company does not exist. Please register first!" });
-    }
-    const passwordMatch = await bcrypt.compare(password, findUser.password);
-    if (!passwordMatch) {
-      return res.status(403).json({ message: "Incorrect email or password" });
+      return res.status(403).json({ message: "Company does not exist. Please register first!" });
     }
     res.status(201).json({ message: "company logged in!", token, company });
     return;
@@ -74,13 +57,9 @@ const updateCompanyInfo = async (req, res) => {
     const company_id = req.company_id;
     companyInfo["logo"] = logo;
 
-    const updateResult = await companyModel.update(companyInfo, {
-      where: { id: company_id },
-    });
-    const findCompany = await companyModel.findAll({
-      where: { id: company_id },
-    });
-    res.status(201).json({ message: "Updated successfully!", findCompany });
+    const updateResult = await companyModel.update(companyInfo, { where: { id: company_id } });
+    const findCompany = await companyModel.findAll({ where: { id: company_id } });
+    res.status(201).json({ message: "Updated successfully!", findCompany })
   } catch (error) {
     res.status(400).json({ message: "failed to update!" });
   }
@@ -97,8 +76,7 @@ const getCompanyAllInfo = async (req, res) => {
         { model: postedJobs, required: false,attributes:{exclude:["id","company_id", "deletedAt"]} }
       ],
     });
-    if (!allCompanyInfo)
-      return req.status(400).json({ message: "No information found!" });
+    if (!allCompanyInfo) return req.status(400).json({ message: "No information found!" });
     res.status(200).json(allCompanyInfo);
   } catch (error) {
     console.log(error);
@@ -128,5 +106,3 @@ export {
   getCompanyAllInfo,
   getAllcompanies
 };
-
-export { registerCompanyController, companyLoginController, updateCompanyInfo, getCompanyAllInfo };
