281fe7203f1f445610b97e9c60eed2e9beb24348
princetindan <wolfsontindan@gmail.com>
2023-06-13 11:15:53 +0000

    itsme

--- Validators/superAdminValidator.js
+++ Validators/superAdminValidator.js
@@ -22,3 +22,5 @@ const superAdminLoginValidator = (req, res, next) => {
     return res.status(400).json({ message: "email and password required!" });}
   next();
 };
+
+export { registersuperAdminValidator, superAdminLoginValidator };
